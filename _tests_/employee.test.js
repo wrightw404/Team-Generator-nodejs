3c6bb16622515e5169fe650ebd34a1ec39b5c740
Will <wrightw404@gmail.com>
2022-09-26 15:00:46 -0400

    able to pass all tests for the employee class

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -11,10 +11,21 @@ test ('test if we can get the constructor parameters', ()=> {
 });
 
 //test for getName()
-
+test ('test if we can get the value for getName', () => {
+    expect(testEmployee.getName()).toBe('Will');
+})
 
 //test for getID()
+test ('test if we can get the value for getID()', () => {
+    expect(testEmployee.getID()).toBe('5047412');
+})
 
 //test for getEmail()
+test ('test if we can get the value for getEmail()', () => {
+    expect(testEmployee.getEmail()).toBe('wrightw404@gmail.com');
+})
 
 //test for getRole()
+test ('test if we can get the value for getRole()', () => {
+    expect(testEmployee.getRole()).toBe('employee');
+})
