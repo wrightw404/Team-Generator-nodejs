4774cb723fd0528344f05c24864820003974e36f
Will <wrightw404@gmail.com>
2022-09-26 14:46:02 -0400

    changed variable name in the class files. Was able to get the first part of the employee test to pass.

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,2 +1,20 @@
-const Employee = require('../lib/employeeClass')
+const employee = require('../lib/employeeClass')
 //create new employee with parameters 
+const testEmployee = new employee ('Will', '5047412', 'wrightw404@gmail.com');
+
+// test to see if you can get constructor parameters 
+
+test ('test if we can get the constructor parameters', ()=> {
+    expect(testEmployee.name).toBe('Will');
+    expect(testEmployee.ID).toBe('5047412');
+    expect(testEmployee.email).toBe('wrightw404@gmail.com');
+});
+
+//test for getName()
+
+
+//test for getID()
+
+//test for getEmail()
+
+//test for getRole()
